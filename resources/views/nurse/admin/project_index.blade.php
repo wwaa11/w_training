0f0e0aa3f1e6b49bd6a4cb4484a53b4a739bc325
Pakawa Kaphondee <pakawa123@gmail.com>
2025-05-14 14:26:55 +0700

    Update UI

--- resources/views/nurse/admin/project_index.blade.php
+++ resources/views/nurse/admin/project_index.blade.php
@@ -1,16 +1,32 @@
 @extends("layouts.nurse")
 @section("content")
     <div class="m-auto w-full p-3 md:w-3/4">
-        <div class="text-2xl font-bold">Training Management</div>
+        <div class="flex">
+            <div class="flex-1 text-2xl font-bold">Training Management</div>
+            <div>
+                <a href="{{ env("APP_URL") }}/nurse/admin/create">
+                    <div class="mb-2 rounded bg-blue-500 p-2 text-white">เพิ่มการฝึกอบรมใหม่</div>
+                </a>
+            </div>
+        </div>
         <hr>
-        @foreach ($projects as $index => $project)
-            <a href="{{ env("APP_URL") }}/nurse/admin/project/{{ $project->id }}">
-                <div class="mt-3 rounded border bg-white">
-                    <div class="p-3 py-6 text-xl font-bold">{{ $index + 1 }} {{ $project->title }}</div>
-                </div>
-            </a>
-        @endforeach
+        <table class="w-full">
+            <tbody>
+                @foreach ($projects as $index => $project)
+                    <tr class="cursor-pointer hover:bg-blue-200" onclick="changePage('{{ env("APP_URL") }}/nurse/admin/project/{{ $project->id }}')">
+                        <td class="p-1">{{ $index + 1 }}. {{ $project->title }}</td>
+                        <td>{{ $project->detail }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+
     </div>
 @endsection
 @section("scripts")
+    <script>
+        function changePage(link) {
+            window.location.href = link;
+        }
+    </script>
 @endsection
